a13c4ed8a3881e0dd19ab0e21b88b56150f0e1de
REBRAND ADMIN <rebrand.com.ua@gmail.com>
2026-03-15 20:57:11 +0200

    finally add handlers

--- components/header.js
+++ components/header.js
@@ -231,11 +231,9 @@ class SiteHeader {
         badge.innerHTML = `
             <div class="badge-content">
                 <span class="badge-icon">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 1C10.3431 1 9 2.34315 9 4V12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12V4C15 2.34315 13.6569 1 12 1Z" fill="#ea580c"/>
-                        <path d="M19 10V12C19 15.866 15.866 19 12 19C8.13401 19 5 15.866 5 12V10" stroke="#ea580c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M12 19V23" stroke="#ea580c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M8 23H16" stroke="#ea580c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ea580c" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 8.5a6.5 6.5 0 1 1 13 0c0 6-6 6-6 10a3.5 3.5 0 1 1-7 0"></path>
+                        <path d="M15 8.5a2.5 2.5 0 0 0-5 0v1a2 2 0 0 1 2 2"></path>
                     </svg>
                 </span>
                 <span class="badge-text">Голосові новини</span>
